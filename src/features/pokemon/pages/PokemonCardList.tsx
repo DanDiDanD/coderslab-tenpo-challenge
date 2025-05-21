015d39fd8775973835a4a3bfdd18f89ab68feb18
DanDiDanD <pfbarrett98@gmail.com>
2025-05-21 02:55:10 -0500

    feat: create useMobile hook to know if viewport is mobile or not

--- src/features/pokemon/pages/PokemonCardList.tsx
+++ src/features/pokemon/pages/PokemonCardList.tsx
@@ -1,9 +1,11 @@
 import { usePokemonCards } from '../api/queries';
 import { PokemonListItem } from '../components/mobile/PokemonListItem';
 import { PokemonTable } from '../components/desktop/PokemonTable';
+import { useMobile } from '../../../hooks';
 
 export const PokemonCardList = () => {
   const { data: pokemonList, isLoading, isError } = usePokemonCards();
+  const isMobile = useMobile();
 
   if (isLoading) return <p>Cargando...</p>;
   if (isError) return <p>Algo salió mal 🧨</p>;
@@ -18,7 +20,11 @@ export const PokemonCardList = () => {
       <div className="p-6">
         <h1 className="text-2xl font-bold mb-4 text-center">Cartas Pokemon</h1>
         <PokemonListItem data={pokemonList?.data || []} />
-        <PokemonTable data={pokemonList?.data || []} />
+        {isMobile ? (
+          <PokemonListItem data={pokemonList?.data || []} />
+        ) : (
+          <PokemonTable data={pokemonList?.data || []} />
+        )}
       </div>
     </>
   );
