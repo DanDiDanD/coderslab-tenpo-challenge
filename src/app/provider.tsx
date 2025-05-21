e247a9899ba02381651b4485c0229c147ebfa4c4
Daniel Angeles <pfbarret98@gmail.com>
2025-05-20 19:56:44 -0500

    feat: add react-error-boundary package

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -6,8 +7,12 @@ const queryClient = new QueryClient();
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </BrowserRouter>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
