401455cdbf29c9231448d53b552a67771438c43b
DanDiDanD <pfbarrett98@gmail.com>
2025-05-21 04:19:37 -0500

    feat(pokemon): improve card list to mobile case

--- src/features/pokemon/components/mobile/PokemonListItem.tsx
+++ src/features/pokemon/components/mobile/PokemonListItem.tsx
@@ -1,4 +1,7 @@
+import { Card } from 'flowbite-react';
+
 import type { PokemonCard } from '../../types';
+import { Image } from '../../../../components';
 
 type PokemonListProps = {
   data: PokemonCard[];
@@ -7,18 +10,24 @@ type PokemonListProps = {
 export const PokemonListItem = ({ data }: PokemonListProps) => (
   <div className="block md:hidden space-y-4">
     {data.map((card) => (
-      <div
-        key={card.id}
-        className="flex gap-4 items-center border p-4 rounded shadow"
-      >
-        <img src={card.images.small} alt={card.name} className="w-20" />
-        <div>
-          <h2 className="font-bold text-lg">{card.name}</h2>
-          <p>HP: {card.hp ?? 'N/A'}</p>
-          <p>Tipo(s): {card.types?.join(', ') ?? 'Desconocido'}</p>
-          <p>Rareza: {card.rarity ?? 'N/A'}</p>
+      <Card>
+        <div
+          key={card.id}
+          className="flex flex-col sm:flex-row gap-4 items-center"
+        >
+          <Image
+            preview={card.images.small}
+            src={card.images.large}
+            alt={card.name}
+          />
+          <div>
+            <h2 className="font-bold text-lg">{card.name}</h2>
+            <p>HP: {card.hp ?? 'N/A'}</p>
+            <p>Tipo(s): {card.types?.join(', ') ?? 'Desconocido'}</p>
+            <p>Rareza: {card.rarity ?? 'N/A'}</p>
+          </div>
         </div>
-      </div>
+      </Card>
     ))}
   </div>
 );
