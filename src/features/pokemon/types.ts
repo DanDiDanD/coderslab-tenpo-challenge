9e742170c33dd5e3001701883569a74d4d05ca6e
DanDiDanD <pfbarrett98@gmail.com>
2025-05-21 15:56:50 -0500

    refactor(pokemon): define pokemon sub types

--- src/features/pokemon/types.ts
+++ src/features/pokemon/types.ts
@@ -1,3 +1,70 @@
+export type PokemonCardRarity =
+  | 'Amazing Rare'
+  | 'Common'
+  | 'LEGEND'
+  | 'Promo'
+  | 'Rare'
+  | 'Rare ACE'
+  | 'Rare BREAK'
+  | 'Rare Holo'
+  | 'Rare Holo EX'
+  | 'Rare Holo GX'
+  | 'Rare Holo LV.X'
+  | 'Rare Holo Star'
+  | 'Rare Holo V'
+  | 'Rare Holo VMAX'
+  | 'Rare Prime'
+  | 'Rare Prism Star'
+  | 'Rare Rainbow'
+  | 'Rare Secret'
+  | 'Rare Shining'
+  | 'Rare Shiny'
+  | 'Rare Shiny GX'
+  | 'Rare Ultra'
+  | 'Uncommon';
+
+export type PokemonCardSuperType = 'Energy' | 'Pokémon' | 'Trainer';
+
+export type PokemonCardSubtype =
+  | 'BREAK'
+  | 'Baby'
+  | 'Basic'
+  | 'EX'
+  | 'GX'
+  | 'Goldenrod Game Corner'
+  | 'Item'
+  | 'LEGEND'
+  | 'Level-Up'
+  | 'MEGA'
+  | 'Pokémon Tool'
+  | 'Pokémon Tool F'
+  | 'Rapid Strike'
+  | 'Restored'
+  | "Rocket's Secret Machine"
+  | 'Single Strike'
+  | 'Special'
+  | 'Stadium'
+  | 'Stage 1'
+  | 'Stage 2'
+  | 'Supporter'
+  | 'TAG TEAM'
+  | 'Technical Machine'
+  | 'V'
+  | 'VMAX';
+
+export type PokemonCardType =
+  | 'Colorless'
+  | 'Darkness'
+  | 'Dragon'
+  | 'Fairy'
+  | 'Fighting'
+  | 'Fire'
+  | 'Grass'
+  | 'Lightning'
+  | 'Metal'
+  | 'Psychic'
+  | 'Water';
+
 export type PokemonCard = {
   id: string;
   name: string;
@@ -6,6 +73,8 @@ export type PokemonCard = {
     large: string;
   };
   hp: number;
-  types: string[];
-  rarity: string;
+  types: PokemonCardType[];
+  subtypes: PokemonCardSubtype[];
+  supertype: PokemonCardSuperType;
+  rarity: PokemonCardRarity;
 };
