3bc0f37b418d017bab63a129d766ce9f3b5c6b52
DanDiDanD <pfbarrett98@gmail.com>
2025-05-21 22:05:17 -0500

    feat(auth): adjust login page to desktop viewport

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -3,7 +3,10 @@ import { Outlet } from 'react-router-dom';
 export const AuthLayout = () => {
   return (
     <div className="min-h-screen flex md:items-center justify-center mt-4 py-8">
-      <main id="main-content" className="w-full max-w-md bg-white mx-3">
+      <main
+        id="main-content"
+        className="bg-white rounded-2xl md:shadow-2xl md:border md:border-gray-200 w-full max-w-md px-6 py-8 mx-4 md:mx-auto"
+      >
         <Outlet />
       </main>
     </div>
